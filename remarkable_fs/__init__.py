cf64bffaec8000f66fa0e0471a8f551c6cf7440e
Nick Smallbone <nick@smallbone.se>
2018-01-19 20:45:42 +0100

    Let mount point be specified.

--- remarkable_fs/__init__.py
+++ remarkable_fs/__init__.py
@@ -2,8 +2,20 @@ from remarkable_fs.sftp import SFTPDirectory, SFTPFile
 from remarkable_fs.connection import connect
 from remarkable_fs.documents import load_documents
 from remarkable_fs.fs import mount
+import sys
+import __builtin__
 
-def main():
+try:
+    raw_input = __builtin__.raw_input
+except AttributeError:
+    raw_input = __builtin__.input
+
+def main(argv = sys.argv):
+    if len(argv) == 1:
+        mount_point = argv[0]
+    else:
+        mount_point = raw_input("Mount point: ")
+
     with connect() as conn:
         dir = SFTPDirectory(conn.sftp, "/home/root/.local/share/remarkable/xochitl")
-        mount("/home/nick/prog/remarkable-fs/mount", load_documents(dir))
+        mount(mount_point, load_documents(dir))
