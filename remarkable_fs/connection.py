3a6f5dbbb76d8104eee81f1cc804f6fe59dcbe34
Nick Smallbone <nick@smallbone.se>
2018-01-19 21:05:47 +0100

    More work on restarting automatically when disconnected

--- remarkable_fs/connection.py
+++ remarkable_fs/connection.py
@@ -4,6 +4,7 @@ from paramiko.client import SSHClient, WarningPolicy
 from paramiko.sftp_client import SFTPClient
 from paramiko.ssh_exception import AuthenticationException
 from getpass import getpass
+from signal import signal, SIGTERM, SIGHUP
 
 Connection = namedtuple('Connection', 'ssh sftp')
 
@@ -20,14 +21,25 @@ def connect():
 
         # Stop xochitl but restart it again if the connection drops
         on_start = "systemctl stop xochitl"
-        on_finish = "systemctl start xochitl"
+        on_finish = "systemctl restart xochitl"
         # We know USB was disconnected when the power supply drops.
         # We also kill the SSH connection so that the information
         # in FUSE is not out of date.
-        stdin, _, _ = ssh.exec_command("""bash -c "trap '%s' EXIT; %s; while udevadm info -p /devices/soc0/soc/2100000.aips-bus/2184000.usb/power_supply/imx_usb_charger | grep -q POWER_SUPPLY_ONLINE=1; do sleep 1; done; %s; kill $PPID" """ % (on_finish, on_start, on_finish))
+        ssh.exec_command(on_start)
+        ssh.exec_command("while udevadm info -p /devices/soc0/soc/2100000.aips-bus/2184000.usb/power_supply/imx_usb_charger | grep -q POWER_SUPPLY_ONLINE=1; do sleep 1; done; %s; kill $PPID" % on_finish)
 
-        with ssh.open_sftp() as sftp:
-            yield Connection(ssh, sftp)
+        try:
+            def raise_exception(*args):
+                raise RuntimeError("Process terminated")
+            signal(SIGTERM, raise_exception)
+            signal(SIGHUP, raise_exception)
+            with ssh.open_sftp() as sftp:
+                yield Connection(ssh, sftp)
 
-        # Closing stdin triggers on_finish to run, so only do it now
-        stdin.close()
+        finally:
+            # Closing stdin triggers on_finish to run, so only do it now
+            print "here"
+            try:
+                ssh.exec_command(on_finish)
+            except:
+                pass
